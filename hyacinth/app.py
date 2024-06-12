26465e3a28a118a634834b99ef14c0cbafe4b748
raza <101937798+razahai@users.noreply.github.com>
2024-06-12 11:20:39 -0400

    feat(app): initialize flask project with tailwind

---
+++ hyacinth/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+def create_app(*, testing=False):
+    app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = "dev"
+    if testing:
+        app.config["TESTING"] = True
+        app.config.from_pyfile("test-config.py")
+    else:
+        app.config.from_pyfile("config.py")
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    return app
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./hyacinth/templates/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
